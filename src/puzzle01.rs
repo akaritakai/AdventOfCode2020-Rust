73f3bab0229343a163db6a82b36e406261535fda
Justin Kaufman <akaritakai@gmail.com>
2020-12-02 17:35:42 -0600

    Add unit tests

--- src/puzzle01.rs
+++ src/puzzle01.rs
@@ -56,3 +56,189 @@ impl Puzzle01 {
         });
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle01::Puzzle01;
+    use std::path::PathBuf;
+    use std::fs;
+
+    #[test]
+    fn test_part_1_example_1() {
+        let input = vec!["1721", "979", "366", "299", "675", "1456"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "514579");
+    }
+
+    #[test]
+    fn test_part_1_example_2() {
+        // Ensure duplicate entries are handled
+        let input = vec!["1000", "1010", "1010"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "1020100");
+    }
+
+    #[test]
+    fn test_part_1_example_3() {
+        // Ensure duplicate entries are handled (alternate order)
+        let input = vec!["1010", "1000", "1010"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "1020100");
+    }
+
+    #[test]
+    fn test_part_1_example_4() {
+        // Ensure a size of 2 is handled
+        let input = vec!["1009", "1011"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "1020099");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_1_example_5() {
+        // Ensure a size of 1 fails
+        let input = vec!["2020"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_1();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_1_example_6() {
+        // Ensure an input that does not satisfy part 1 fails
+        let input = vec!["1010", "1011"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_1();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_1_example_7() {
+        // Ensure an input that does not satisfy part 1 fails (alternate)
+        let input = vec!["1721", "979", "366", "675", "1456"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_1();
+    }
+
+    #[test]
+    fn test_part_1_example_8() {
+        // Ensure a size of 2 with duplicates are handled
+        let input = vec!["1010", "1010"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "1020100");
+    }
+
+    #[test]
+    fn test_part_1_example_9() {
+        // Ensure a pass if the subset is on the extremes of the array
+        let input = vec!["1721", "979", "366", "675", "1456", "299"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "514579");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_1_example_10() {
+        // Ensure an empty input fails
+        let puzzle = Puzzle01::create("");
+        puzzle.solve_part_1();
+    }
+
+    #[test]
+    fn test_solve_part_1() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/1")).unwrap();
+        let puzzle = Puzzle01::create(input.as_str());
+        assert_eq!(puzzle.solve_part_1(), "440979");
+    }
+
+    #[test]
+    fn test_part_2_example_1() {
+        let input = vec!["1721", "979", "366", "299", "675", "1456"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "241861950");
+    }
+
+    #[test]
+    fn test_part_2_example_2() {
+        // Ensure duplicate entries are handled
+        let input = vec!["672", "672", "676"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "305270784");
+    }
+
+    #[test]
+    fn test_part_2_example_3() {
+        // Ensure duplicate entries are handled (alternate order)
+        let input = vec!["672", "676", "672"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "305270784");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_2_example_4() {
+        // Ensure a size of 2 fails
+        let input = vec!["1009", "1011"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_2();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_2_example_5() {
+        // Ensure a size of 1 fails
+        let input = vec!["2020"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_2();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_2_example_6() {
+        // Ensure an input that does not satisfy part 2 fails
+        let input = vec!["500", "501", "1020"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_2();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_2_example_7() {
+        // Ensure an input that does not satisfy part 2 fails (alternate)
+        let input = vec!["1721", "979", "366", "299", "1456"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        puzzle.solve_part_2();
+    }
+
+    #[test]
+    fn test_part_2_example_8() {
+        // Ensure a size of 4 with duplicates are handled
+        let input = vec!["672", "500", "672", "676"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "305270784");
+    }
+
+    #[test]
+    fn test_part_2_example_9() {
+        // Ensure a pass if the subset is on the extremes of the array
+        let input = vec!["979", "1721", "1456", "366", "299", "675"];
+        let puzzle = Puzzle01::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "241861950");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_part_2_example_10() {
+        // Ensure an empty input fails
+        let puzzle = Puzzle01::create("");
+        puzzle.solve_part_2();
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/1")).unwrap();
+        let puzzle = Puzzle01::create(input.as_str());
+        assert_eq!(puzzle.solve_part_2(), "82498112");
+    }
+}
--- src/puzzle02.rs
+++ src/puzzle02.rs
@@ -50,3 +50,100 @@ fn parse(line: &str) -> (usize, usize, char, String) {
             cap[3].parse::<char>().unwrap(),
             cap[4].to_string());
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle02::Puzzle02;
+    use std::path::PathBuf;
+    use std::fs;
+
+    #[test]
+    fn test_part_1_example_1() {
+        let input = vec![
+            "1-3 a: abcde",
+            "1-3 b: cdefg",
+            "2-9 c: ccccccccc"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "2");
+    }
+
+    #[test]
+    fn test_part_1_example_2() {
+        let input = vec![
+            "1-3 a: abcde"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "1");
+    }
+
+    #[test]
+    fn test_part_1_example_3() {
+        let input = vec![
+            "1-3 b: cdefg"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "0");
+    }
+
+    #[test]
+    fn test_part_1_example_4() {
+        let input = vec![
+            "2-9 c: ccccccccc"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_1(), "1");
+    }
+
+    #[test]
+    fn test_solve_part_1() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/2")).unwrap();
+        let puzzle = Puzzle02::create(input.as_str());
+        assert_eq!(puzzle.solve_part_1(), "434");
+    }
+
+    #[test]
+    fn test_part_2_example_1() {
+        let input = vec![
+            "1-3 a: abcde",
+            "1-3 b: cdefg",
+            "2-9 c: ccccccccc"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "1");
+    }
+
+    #[test]
+    fn test_part_2_example_2() {
+        let input = vec![
+            "1-3 a: abcde"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "1");
+    }
+
+    #[test]
+    fn test_part_2_example_3() {
+        let input = vec![
+            "1-3 b: cdefg"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "0");
+    }
+
+    #[test]
+    fn test_part_2_example_4() {
+        let input = vec![
+            "2-9 c: ccccccccc"
+        ];
+        let puzzle = Puzzle02::create(&input.join("\n"));
+        assert_eq!(puzzle.solve_part_2(), "0");
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/2")).unwrap();
+        let puzzle = Puzzle02::create(input.as_str());
+        assert_eq!(puzzle.solve_part_2(), "509");
+    }
+}
