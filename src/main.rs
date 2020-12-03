b9b8629c841a54ca4fd526d1a43ff26b476f82df
Justin Kaufman <akaritakai@gmail.com>
2020-12-03 09:09:30 -0600

    Day 3 solution

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,20 @@ use crate::puzzle_input_fetcher::PuzzleInputFetcher;
 use crate::puzzle::AbstractPuzzle;
 use crate::puzzle01::Puzzle01;
 use crate::puzzle02::Puzzle02;
+use crate::puzzle03::Puzzle03;
 
 mod puzzle_input_fetcher;
 mod puzzle;
 mod puzzle01;
 mod puzzle02;
+mod puzzle03;
 
 fn main() {
     let mut fetcher = PuzzleInputFetcher::create();
     let puzzles : Vec<Box<dyn AbstractPuzzle>> = vec![
         Puzzle01::create(fetcher.get_puzzle_input(1).unwrap()),
-        Puzzle02::create(fetcher.get_puzzle_input(2).unwrap())
+        Puzzle02::create(fetcher.get_puzzle_input(2).unwrap()),
+        Puzzle03::create(fetcher.get_puzzle_input(3).unwrap())
     ];
     for puzzle in puzzles.iter() {
         let day = format!("{:02}", puzzle.get_day());
---
+++ src/puzzle03.rs
@@ -0,0 +1,96 @@
+use crate::puzzle::AbstractPuzzle;
+
+pub struct Puzzle03 {
+    input: String,
+}
+
+impl AbstractPuzzle for Puzzle03 {
+    fn get_day(&self) -> u8 {
+        return 3;
+    }
+
+    fn solve_part_1(&self) -> String {
+        return self.trees_on_slope(3, 1).to_string();
+    }
+
+    fn solve_part_2(&self) -> String {
+        let mut count = 1;
+        count *= self.trees_on_slope(1, 1);
+        count *= self.trees_on_slope(3, 1);
+        count *= self.trees_on_slope(5, 1);
+        count *= self.trees_on_slope(7, 1);
+        count *= self.trees_on_slope(1, 2);
+        return count.to_string();
+    }
+}
+
+impl Puzzle03 {
+    pub fn create(input: &str) -> Box<dyn AbstractPuzzle> {
+        return Box::new(Puzzle03 {
+            input: input.to_string()
+        });
+    }
+
+    fn trees_on_slope(&self, x: usize, y: usize) -> usize {
+        return self.input.lines()
+            .step_by(y)
+            .enumerate()
+            .filter(|(i, line)| line.chars().cycle().nth(i * x) == Some('#'))
+            .count();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle03::Puzzle03;
+    use std::path::PathBuf;
+    use std::fs;
+
+    #[test]
+    fn test_part_1_example_1() {
+        let input = "..##.......\n\
+                           #...#...#..\n\
+                           .#....#..#.\n\
+                           ..#.#...#.#\n\
+                           .#...##..#.\n\
+                           ..#.##.....\n\
+                           .#.#.#....#\n\
+                           .#........#\n\
+                           #.##...#...\n\
+                           #...##....#\n\
+                           .#..#...#.#\n";
+        let puzzle = Puzzle03::create(input);
+        assert_eq!(puzzle.solve_part_1(), "7");
+    }
+
+    #[test]
+    fn test_solve_part_1() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/3")).unwrap();
+        let puzzle = Puzzle03::create(input.as_str());
+        assert_eq!(puzzle.solve_part_1(), "284");
+    }
+
+    #[test]
+    fn test_part_2_example_1() {
+        let input = "..##.......\n\
+                           #...#...#..\n\
+                           .#....#..#.\n\
+                           ..#.#...#.#\n\
+                           .#...##..#.\n\
+                           ..#.##.....\n\
+                           .#.#.#....#\n\
+                           .#........#\n\
+                           #.##...#...\n\
+                           #...##....#\n\
+                           .#..#...#.#\n";
+        let puzzle = Puzzle03::create(input);
+        assert_eq!(puzzle.solve_part_2(), "336");
+    }
+
+    #[test]
+    fn test_solve_part_2() {
+        let input = fs::read_to_string(PathBuf::from("resources/tests/3")).unwrap();
+        let puzzle = Puzzle03::create(input.as_str());
+        assert_eq!(puzzle.solve_part_2(), "3510149120");
+    }
+}
